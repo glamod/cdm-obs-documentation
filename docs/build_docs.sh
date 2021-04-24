0b15cce4d1d0719268fb015249f8261077fc2572
DavidBerryNOC <dyb@noc.ac.uk>
2021-04-24 15:45:56 +0100

    addition of conceptual data model figure

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,7 +2,7 @@
 
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme dot
 
 # Build documentation
 # ===================
